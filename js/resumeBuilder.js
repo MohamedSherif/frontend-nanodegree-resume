223b82df13dfad3a3636d85066358696942d82e6
Mohamed Sherif <mohamed.sherif.2012@gmail.com>
2016-09-15 13:22:54 +0200

    Edit the resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+  $("#main").append("Mohamed Sherif");
+
+  var awesomeThoughts = "I am Mohamed and I am AWESOME!";
+  console.log(awesomeThoughts);
